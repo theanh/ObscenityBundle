ce6f986ca571ab71abede9720ef914a4ed81bc5e
The Anh Nguyen <theanh.huy@gmail.com>
2015-08-03 13:57:24 +0700

    Reconfig composer.

--- tests/ObscenityTest.php
+++ tests/ObscenityTest.php
@@ -1,14 +1,23 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
-require_once Talu\ObscenityBundle\Obscenity;
 
-// require_once __DIR__ . '/../src/Obscenity.php';
+namespace Talu\ObscenityBundle\Tests;
 
-class ObscenityTest extends PHPUnit_Framework_TestCase
+$file = __DIR__.'/../vendor/autoload.php';
+if (!file_exists($file)) {
+    throw new RuntimeException('Install dependencies to run test suite.');
+}
+$autoload = require_once $file;
+
+require_once __DIR__ . '/../src/Obscenity.php';
+
+use Talu\ObscenityBundle\Obscenity;
+
+class ObscenityTest extends \PHPUnit_Framework_TestCase
 {
     public function testProfane()
     {
         $ob = new Obscenity();
+
         $arrTestCases = [
             [null => false],
             ['hell??' => true],
